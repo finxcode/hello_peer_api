fc7834d539db7ca6925b92bef63b5fd10cce9106
finxcode <f.sheng@outlook.com>
2022-04-20 14:41:56 +0800

    fix set user details api spec

--- controllers/user.go
+++ controllers/user.go
@@ -52,25 +52,25 @@ type UserDetails struct {
 }
 
 type UserDetailsUpdate struct {
-	UserName      string   `example:"豆豆"`
-	Age           int      `example:"25"`
-	Occupation    string   `example:"平面设计师"`
-	Constellation string   `example:"处女座"`
-	Height        string   `example:"165cm"`
-	Weight        string   `example:"43kg"`
-	Education     string   `example:"本科"`
-	Location      string   `example:"深圳"`
-	Hometown      string   `example:"湖南长沙"`
-	SelfDesc      string   `example:"自我描述"`
-	Hobbies       string   `example:"兴趣爱好"`
-	Declaration   string   `example:"交友宣言"`
-	TheOne        string   `example:"希望另一半的样子"`
-	Tags          []string `example:"猫控,读书达人,电影爱好者,旅行者"`
-	Birthday      string   `json:"birthday" example:"1988-10-2"`
-	Purpose       string   `json:"purpose" example:"交友"`
-	Gender        int      `json:"gender" example:"1"`
-	Marriage      string   `json:"marriage" example:"未婚"`
-	Income        string   `json:"income" example:"15-25w"`
+	UserName      string `example:"豆豆"`
+	Age           int    `example:"25"`
+	Occupation    string `example:"平面设计师"`
+	Constellation string `example:"处女座"`
+	Height        string `example:"165cm"`
+	Weight        string `example:"43kg"`
+	Education     string `example:"本科"`
+	Location      string `example:"深圳"`
+	Hometown      string `example:"湖南长沙"`
+	SelfDesc      string `example:"自我描述"`
+	Hobbies       string `example:"兴趣爱好"`
+	Declaration   string `example:"交友宣言"`
+	TheOne        string `example:"希望另一半的样子"`
+	Tags          string `example:"猫控 读书达人 电影爱好者 旅行者"`
+	Birthday      string `json:"birthday" example:"1988-10-2"`
+	Purpose       string `json:"purpose" example:"交友"`
+	Gender        int    `json:"gender" example:"1"`
+	Marriage      string `json:"marriage" example:"未婚"`
+	Income        string `json:"income" example:"15-25w"`
 }
 
 // GetRandomUsersInSquare
