e88a538737891d2d41a57809f68c15861856fcdc
finxcode <11415401+ehangnet@user.noreply.gitee.com>
2022-09-13 14:27:09 +0800

    add getViewList api spec

--- controllers/relation/view.go
+++ controllers/relation/view.go
@@ -16,3 +16,18 @@ import "github.com/gin-gonic/gin"
 func UpdateAllNewViewStatus(c *gin.Context) {
 	//设置用户新增看过已阅
 }
+
+// GetViewList
+// @Summary 获取用户关注列表
+// @Description 可获取用户关注列表 status说明： 0 - 新关注（未揭秘） 1 - 已阅关注（未揭秘） 2 - 已揭秘
+// @ID get_view_list
+// @Tags Relation
+// @Accept application/json
+// @Produce application/json
+// @Security x-token
+// @param x-token header string true "Authorization"
+// @Success 0 {array} models.View
+// @Router /user/relation/getViewList [get]
+func GetViewList(c *gin.Context) {
+	//获取用户关注列表
+}
--- models/relation.go
+++ models/relation.go
@@ -19,3 +19,16 @@ type Fan struct {
 type ViewRequest struct {
 	On string `json:"on" example:"2"`
 }
+
+type View struct {
+	Id         int    `json:"uid" example:"1"`
+	UserName   string `json:"username" example:"香蕉苹果"`
+	PetName    string `json:"petName" example:"一个宠物"`
+	Age        int    `json:"age" example:"26"`
+	Location   string `json:"location" example:"广东，深圳"`
+	Occupation string `json:"occupation" example:"产品经理"`
+	Images     string `json:"coverImage" example:"https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTL13ic0iaA0ffWldrLjv9Ou02CuJCcjuKJ7rAzatVEzEUsrceUEdIuSiaR7bnicf5X2puMFRNDLrPEJlw/132"`
+	Status     int    `json:"status" example:"1"`
+	Message    string `json:"message" example:"Ta好像对你很感兴趣"`
+	Highlight  string `json:"highlight" example:"感兴趣"`
+}
